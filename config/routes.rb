a561de278cdb2207c0b02b8081ae707fe8dd6fd7
Eddie Drury <eddie.drury@gmail.com>
2024-01-04 06:42:57 +0800

    implementing Bulma

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  root "predictors#index"
+  root "predictions#index"
 
   resources :predictors, only: [:index, :show, :new, :create] do
     resources :predictions, only: [:new, :create]
