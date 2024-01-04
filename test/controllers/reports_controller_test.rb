7d5c6f624ebbf35b216d1c28cb2b9ad686b2d81b
Eddie Drury <eddie.drury@gmail.com>
2024-01-04 12:51:52 +0800

    report tests

--- test/controllers/reports_controller_test.rb
+++ test/controllers/reports_controller_test.rb
@@ -7,24 +7,26 @@ class ReportsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @report = reports(:one)
     @prediction = predictions(:one)
-    @user = users(:one)
-    @user.confirm
+    @adminuser = users(:one)
+    @nonadminuser = users(:two)
+    @adminuser.confirm
+    @nonadminuser.confirm
   end
 
   test "should get index" do
-    sign_in @user
+    sign_in @adminuser
     get reports_url
     assert_response :success
   end
 
   test "should get new" do
-    sign_in @user
+    sign_in @adminuser
     get new_prediction_report_url(@prediction)
     assert_response :success
   end
 
   test "should create report" do
-    sign_in @user
+    sign_in @adminuser
     assert_difference("Report.count") do
       post prediction_reports_url(@prediction), params: { report: { body: @report.body, reason: @report.reason, status: @report.status, prediction_id: @prediction.id } }
     end
@@ -32,23 +34,34 @@ class ReportsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should show report" do
-    sign_in @user
+    sign_in @adminuser
     get report_url(@report)
     assert_response :success
   end
 
   test "should get edit" do
-    sign_in @user
+    sign_in @adminuser
     get edit_report_url(@report)
     assert_response :success
   end
 
   test "should destroy report" do
-    sign_in @user
+    sign_in @adminuser
     assert_difference("Report.count", -1) do
       delete report_url(@report)
     end
-
     assert_redirected_to reports_url
   end
+
+  test "should not show report if not logged in" do
+    get report_path(@report)
+    assert_redirected_to new_user_session_path
+  end
+
+  test "should not show others report if not admin" do
+    sign_in @nonadminuser
+    get report_url(@report)
+    assert_redirected_to root_path
+  end
+
 end
