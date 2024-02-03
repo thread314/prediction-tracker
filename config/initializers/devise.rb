7d1a8decc7e444792d5a7bd56036df33897f0d32
Eddie Drury <eddie.drury@gmail.com>
2024-02-03 21:32:20 +0800

    fix devise from email take 2

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -24,7 +24,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'noreply@salty-sea-96190-e9acf7b934b2.herokuapp.com'
+  config.mailer_sender = 'prediction.track@gmail.com'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
