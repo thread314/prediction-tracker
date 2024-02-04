1641bc917f5e30d2f6d73b1ab3f12f1ca1ca555d
Eddie Drury <eddie.drury@gmail.com>
2024-02-04 11:23:35 +0800

    fix devise from email take 5

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -24,7 +24,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = env['CLOUDMAILIN_FORWARD_ADDRESS']
+  config.mailer_sender = 'e95dad66bb34fc273631@cloudmailin.net'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
